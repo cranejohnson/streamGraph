dc5f6e20c2e5e4eb781042811ba6c1257433c407
Crane Johnson <crane@whitewinter.net>
2016-06-01 01:03:00 -0800

    working update...reformulated how AHPS data is loaded

--- get_ahps_data.php
+++ get_ahps_data.php
@@ -1,6 +1,97 @@
 <?
 //Simple script to get ahps data and avoid cross site request
 $site = $_GET['site'];
-$ahps_xml = $homepage = file_get_contents("http://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=$site&output=xml");
-echo $ahps_xml;
+$format = 'xml';
+if(isset($_GET['format'])){
+    $format = $_GET['format'];
+}
+
+$xml_string = file_get_contents("http://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=$site&output=xml");
+
+if($format == 'xml'){
+    echo $xml_string;
+}
+if($format == 'json'){
+    $xml = simplexml_load_string($xml_string);
+    $array= xmlToArray($xml);
+    echo json_encode($array);
+}
+
+
+function xmlToArray($xml, $options = array()) {
+    $defaults = array(
+        'namespaceSeparator' => ':',//you may want this to be something other than a colon
+        'attributePrefix' => '_',   //to distinguish between attributes and nodes with the same name
+        'alwaysArray' => array(),   //array of xml tag names which should always become arrays
+        'autoArray' => true,        //only create arrays for tags which appear more than once
+        'textContent' => 'value',       //key used for the text content of elements
+        'autoText' => true,         //skip textContent key if node has no attributes or child nodes
+        'keySearch' => false,       //optional search and replace on tag and attribute names
+        'keyReplace' => false       //replace values for above search values (as passed to str_replace())
+    );
+    $options = array_merge($defaults, $options);
+    $namespaces = $xml->getDocNamespaces();
+    $namespaces[''] = null; //add base (empty) namespace
+
+    //get attributes from all namespaces
+    $attributesArray = array();
+    foreach ($namespaces as $prefix => $namespace) {
+        foreach ($xml->attributes($namespace) as $attributeName => $attribute) {
+            //replace characters in attribute name
+            if ($options['keySearch']) $attributeName =
+                    str_replace($options['keySearch'], $options['keyReplace'], $attributeName);
+            $attributeKey = $options['attributePrefix']
+                    . ($prefix ? $prefix . $options['namespaceSeparator'] : '')
+                    . $attributeName;
+            $attributesArray[$attributeKey] = (string)$attribute;
+        }
+    }
+
+    //get child nodes from all namespaces
+    $tagsArray = array();
+    foreach ($namespaces as $prefix => $namespace) {
+        foreach ($xml->children($namespace) as $childXml) {
+            //recurse into child nodes
+            $childArray = xmlToArray($childXml, $options);
+            list($childTagName, $childProperties) = each($childArray);
+
+            //replace characters in tag name
+            if ($options['keySearch']) $childTagName =
+                    str_replace($options['keySearch'], $options['keyReplace'], $childTagName);
+            //add namespace prefix, if any
+            if ($prefix) $childTagName = $prefix . $options['namespaceSeparator'] . $childTagName;
+
+            if (!isset($tagsArray[$childTagName])) {
+                //only entry with this key
+                //test if tags of this type should always be arrays, no matter the element count
+                $tagsArray[$childTagName] =
+                        in_array($childTagName, $options['alwaysArray']) || !$options['autoArray']
+                        ? array($childProperties) : $childProperties;
+            } elseif (
+                is_array($tagsArray[$childTagName]) && array_keys($tagsArray[$childTagName])
+                === range(0, count($tagsArray[$childTagName]) - 1)
+            ) {
+                //key already exists and is integer indexed array
+                $tagsArray[$childTagName][] = $childProperties;
+            } else {
+                //key exists so convert to integer indexed array with previous value in position 0
+                $tagsArray[$childTagName] = array($tagsArray[$childTagName], $childProperties);
+            }
+        }
+    }
+
+    //get text content of node
+    $textContentArray = array();
+    $plainText = trim((string)$xml);
+    if ($plainText !== '') $textContentArray[$options['textContent']] = $plainText;
+
+    //stick it all together
+    $propertiesArray = !$options['autoText'] || $attributesArray || $tagsArray || ($plainText === '')
+            ? array_merge($attributesArray, $tagsArray, $textContentArray) : $plainText;
+
+    //return node as array
+    return array(
+        $xml->getName() => $propertiesArray
+    );
+}
 ?>
