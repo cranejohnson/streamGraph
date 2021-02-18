297f67d646c735963ec7da28e9dcef6b4d47b10a
Crane Johnson <crane@whitewinter.net>
2021-02-17 21:53:02 -0900

    Updated to match new usgs peak flow nomenclature

--- get_peak_flow.php
+++ get_peak_flow.php
@@ -1,58 +1,121 @@
 <?php
-//Simple script to get ahps data and avoid cross site request
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+
 $site = $_GET['site'];
 
-$string = file_get_contents("https://streamstatsags.cr.usgs.gov/gagepages/html/".$site.".htm");
 
-$json = array();
+$string = @file_get_contents("https://streamstatsags.cr.usgs.gov/gagepages/html/".$site.".htm");
 
-if(preg_match("/Weighted_5_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['wtd5'] = $output_array[1];
-}
-if(preg_match("/Weighted_10_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['wtd10'] = $output_array[1];
-}
-if(preg_match("/Weighted_25_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['wtd25'] = $output_array[1];
-}
-if(preg_match("/Weighted_50_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['wtd50'] = $output_array[1];
-}
-if(preg_match("/Weighted_100_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['wtd100'] = $output_array[1];
-}
-if(preg_match("/Weighted_200_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['wtd200'] = $output_array[1];
-}
-if(preg_match("/Weighted_500_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['wtd500'] = $output_array[1];
+if(!$string) {
+  header('HTTP/1.1 500 Internal Server Error');
+  exit;
+}  
+
+$dom = new domDocument;
+
+@$dom->loadHTML($string);
+$dom->preserveWhiteSpace = false;
+$tables = $dom->getElementsByTagName('table');
+
+print_r($tables);
+
+$rows = $tables->item(2)->getElementsByTagName('tr');
+
+$tableData = array();
+
+foreach ($rows as $row) {
+	$cols = $row->getElementsByTagName('td');
+  	if($cols[4]->nodeValue == 'Y'){
+      $tableData[$cols[0]->nodeValue] = $cols[1]->nodeValue;
+    }  
 }
 
-if(preg_match("/W5_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['sta5'] = $output_array[1];
-}
-if(preg_match("/10_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['sta10'] = $output_array[1];
-}
-if(preg_match("/W25_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['sta25'] = $output_array[1];
-}
-if(preg_match("/50_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['sta50'] = $output_array[1];
-}
-if(preg_match("/100_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['sta100'] = $output_array[1];
-}
-if(preg_match("/200_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['sta200'] = $output_array[1];
-}
-if(preg_match("/500_Year_Peak_Flood<\/td>\s*<td>(.*)<\/td>/U", $string, $output_array)){
-    $json['sta500'] = $output_array[1];
-}
+
+function extractData($tableData){
+
+    $json = array();
+
+    foreach($tableData as $key => $flow){
+
+      	if($key == "Weighted_20_percent_AEP_flood"){
+            $json['wtd5'] = $flow;
+        }
+      	if($key == "Weighted_10_percent_AEP_flood"){
+            $json['wtd10'] = $flow;
+        }
+      	if($key == "Weighted_4_percent_AEP_flood"){
+            $json['wtd25'] = $flow;
+        }
+      	if($key == "Weighted_2_percent_AEP_flood"){
+            $json['wtd50'] = $flow;
+        }
+      	if($key == "Weighted_1_percent_AEP_flood"){
+            $json['wtd100'] = $flow;
+        }
+      	if($key == "Weighted_0_5_percent_AEP_flood"){
+            $json['wtd200'] = $flow;
+        }
+      	if($key == "Weighted_0_2_percent_AEP_flood"){
+            $json['wtd500'] = $flow;
+        }
+      	if($key == "50_percent_AEP_flood"){
+            $json['sta2'] = $flow;
+        }
+      	if($key == "20_percent_AEP_flood"){
+            $json['sta5'] = $flow;
+        }
+      	if($key == "10_percent_AEP_flood"){
+            $json['sta10'] = $flow;
+        }
+      	if($key == "4_percent_AEP_flood"){
+            $json['sta25'] = $flow;
+        }
+      	if($key == "2_percent_AEP_flood"){
+            $json['sta50'] = $flow;
+        }
+      	if($key == "1_percent_AEP_flood"){
+            $json['sta100'] = $flow;
+        }
+      	if($key == "0_5_percent_AEP_flood"){
+            $json['sta200'] = $flow;
+        }
+      	if($key == "0_2_percent_AEP_flood"){
+            $json['sta500'] = $flow;
+        }
+      	if($key == "Urban_50_percent_AEP_flood"){
+            $json['sta2'] = $flow;
+        }
+      	if($key == "Urban_20_percent_AEP_flood"){
+            $json['sta5'] = $flow;
+        }
+      	if($key == "Urban_10_percent_AEP_flood"){
+            $json['sta10'] = $flow;
+        }
+      	if($key == "Urban_4_percent_AEP_flood"){
+            $json['sta25'] = $flow;
+        }
+      	if($key == "Urban_2_percent_AEP_flood"){
+            $json['sta50'] = $flow;
+        }
+      	if($key == "Urban_1_percent_AEP_flood"){
+            $json['sta100'] = $flow;
+        }
+      	if($key == "Urban_0_5_percent_AEP_flood"){
+            $json['sta200'] = $flow;
+        }
+      	if($key == "Urban_0_2_percent_AEP_flood"){
+            $json['sta500'] = $flow;
+        }
+     
+    }
+  	return $json;
+
+}    
 
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
-echo json_encode($json);
+echo json_encode(extractData($tableData));
 
 
 ?>
